0dfeca610ff2c7382caed64bd5b2514e747fe1df
Shad M Khan <sm.khan2010@gmail.com>
2017-02-12 02:00:16 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,27 +1,20 @@
 /*
 * Shad M Khan
 * Binet theory implemented in Java to find the nth fibonacci number
-* 2nd February 2017
+* 12th February 2017
 */
 
 //Input n through console
 // 0>n>10^9
 
 import java.io.*;
-import java.math.BigInteger;
+
+import java.util.Scanner;
 public class Fibonacci {
 	public static void main(String[] args) {
-		int n=0;
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			 n = Integer.parseInt( br.readLine());
-
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}	
-		
-		System.out.println(fibBINET(n));
+		Scanner scanner= new Scanner(System.in);
+		long n = scanner.nextLong();
+		//System.out.println(fibBINET(n));
 
 		for (long i = 1; i <= n; i++) {
 			System.out.println(fibBINET(i));
@@ -35,6 +28,6 @@ public class Fibonacci {
 	 {
 	    double sqrt5 = Math.sqrt(5);
 	    double phi=(1+sqrt5)/2;
-	    return (long) (Math.round((Math.pow(phi, n)/sqrt5));
+	    return (long) (Math.round((Math.pow(phi, n)/sqrt5)));
 	 }
 }
