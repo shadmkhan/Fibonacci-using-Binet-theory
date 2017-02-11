76006f6b5fe6adab27f3af1f43e075cff3d48b1e
Shad M Khan <sm.khan2010@gmail.com>
2017-02-12 01:57:41 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -35,6 +35,6 @@ public class Fibonacci {
 	 {
 	    double sqrt5 = Math.sqrt(5);
 	    double phi=(1+sqrt5)/2;
-	    return (long) (Math.pow(phi, n)/sqrt5);
+	    return (long) (Math.round((Math.pow(phi, n)/sqrt5));
 	 }
 }
